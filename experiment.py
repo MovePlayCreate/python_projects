a1209e9c6c5a89aa9f00aa088554deb688afd890
MovePlayCreate <movecreateplay@gmail.com>
2025-08-17 20:40:56 -0600

    Create experiment.py
    Testing isprime on my phone

---
+++ experiment.py
@@ -0,0 +1,14 @@
+def is_prime(num):
+    check_until = num ** .5
+    if num < 2:
+        return False
+    if num == 2:
+        return True
+    for i in range(3, int(check_until), 2):
+        if num % i == 0:
+            return False
+    return True
+    
+is_prime(27)
+is_prime(29)
+                
