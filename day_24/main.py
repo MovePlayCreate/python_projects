1b51d5d54cb505f56387062657e9fe3ebfeafb7a
MovePlayCreate <movecreateplay@gmail.com>
2025-08-29 16:19:04 -0600

    Update main.py

--- day_24/main.py
+++ day_24/main.py
@@ -10,6 +10,8 @@ with open("../../../Desktop/my_file.txt", mode="r") as file:
     contents = file.read()
     print(contents)
     
+# test this to see if it worked later.
+# written while in a Lyft to Boise airport
 with open("../../../Desktop/my_file.txt", mode="w") as data_file:
     data_file.write("working?") 
     
