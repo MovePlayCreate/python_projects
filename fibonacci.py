0fbda5619e9c945e15eb98243ec318818de4f578
MovePlayCreate <movecreateplay@gmail.com>
2025-08-30 05:06:49 -0600

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,11 @@
-def fibonacci(a, b, how_many):
-    nums = []
+def fibonacci(a, b, how_many, nums):
     nums.append(a)
     for i in range(how_many):
-        fibonacci(b, a + b)
+        fibonacci(b, a + b) 
+    return nums
         
-fibonacci(0, 1, 11)
-        
+fibonacci(0, 1, 11, [])
+#check this solution with python editor later        
         
     
  
